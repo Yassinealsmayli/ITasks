7f297ace6767c364c531ad44363fccf92025f9de
yass sm <Yassinealsmayli>
2025-08-01 19:46:55 +0300

    v0.0.2 Database structure and Services added

---
+++ Models/Tasks.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ITasks.Models
+{
+    public class ITask
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TaskID { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; } = "";
+
+        public DateTime DeadLine { get; set; }
+    }
+}
---
+++ Services/ITaskService.cs
@@ -0,0 +1,79 @@
+﻿using ITasks.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace ITasks.Services
+{
+    public interface ITaskService
+    {
+        ValueTask<List<ITask>> GetUserTasks(int UID);
+
+        ValueTask<ITask?> GetTask(int TaskID);
+
+        Task<int> AddTask(ITask task);
+
+        Task<ITask> UpdateTask(int TaskID, ITask task);
+
+        Task<ITask?> RemoveTask(int UID,int TaskID);
+    }
+
+    public class TaskService : ITaskService
+    {
+        private AppDbContext _context;
+
+        public TaskService(AppDbContext context) {  _context = context; }
+
+        public async Task<int> AddTask(ITask task)
+        {
+            ITask t = _context.Tasks.AddAsync(task).Result.Entity;
+            await _context.SaveChangesAsync();
+            return t.TaskID;
+        }
+
+        public async ValueTask<ITask?> GetTask(int TaskID)
+        {
+            return await _context.Tasks.FindAsync(TaskID);
+        }
+
+        public async ValueTask<List<ITask>> GetUserTasks(int UID)
+        {
+            List<UserTask> ut = await _context.userTasks.Where(t => t.UID == UID).ToListAsync();
+            List<ITask> tasks = new List<ITask>();
+            foreach (UserTask item in ut)
+            {
+                tasks.Add(_context.Tasks.Find(item.TaskID));
+            }
+            return tasks;
+        }
+
+        public async Task<ITask?> RemoveTask(int UID,int TaskID)
+        {
+            ITask? res = _context.Tasks.FindAsync(TaskID).Result;
+            UserTask? ut = _context.userTasks.Where(i => i.TaskID == TaskID && i.UID == UID).First();
+            _context.userTasks.Remove(ut);
+            await _context.SaveChangesAsync();
+            if (await _context.userTasks.FindAsync(TaskID) == null)
+            {
+                
+                if (res == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    _context.Tasks.Remove(res);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            return res;
+        }
+
+        public async Task<ITask> UpdateTask(int TaskID, ITask task)
+        {
+            task.TaskID = TaskID;
+            _context.Tasks.Update(task);
+            await _context.SaveChangesAsync();
+            return task;
+        }
+    }
+}
